b823569a57d13fe4435525d53734caca1b4ce07f
gavink97 <gavin.kondrath@gmail.com>
2023-08-01 01:09:20 +0700

    improved image handling

--- scripts/to_mysql.py
+++ scripts/to_mysql.py
@@ -1,24 +1,20 @@
 import mysql.connector
 import pandas as pd
 import os
-from dotenv import load_dotenv
 import time
-
-load_dotenv()
-db_host = os.environ['MYSQL_HOST']
-db_user = os.environ['MYSQL_USER']
-db_pass = os.environ['MYSQL_PASSWORD']
-db_database = os.environ['MYSQL_DB']
 
 db = mysql.connector.connect(
     user=f'{db_user}',
     password=f'{db_pass}',
     host=f'{db_host}',
+    port=f'{db_port}',
     database=f'{db_database}'
 )
 
 folder_path = 'sheets'
 file_names = os.listdir(folder_path)
+
+print(f"Writing to: {db_database}")
 
 cursor = db.cursor()
 
@@ -29,11 +25,11 @@ for file_name in file_names:
         columns = ", ".join([f"{col} VARCHAR(255)" for col in df.columns])
         drop_query = f"DROP TABLE IF EXISTS {source_name};"
         create_table_query = f"CREATE TABLE IF NOT EXISTS {source_name} ({columns}, id INT AUTO_INCREMENT PRIMARY KEY);"
-        
+
         cursor.execute(drop_query)
         print(f"dropped old {source_name} table")
         time.sleep(1)
-    
+
         cursor.execute(create_table_query)
         print(f"Table {source_name} created")
 
