30dabf59dae27ae2ef7f1a155766620b242e38d5
gavink97 <gavin.kondrath@gmail.com>
2023-08-26 21:37:58 +0800

    updated xpaths

--- scripts/filter_csv.py
+++ scripts/filter_csv.py
@@ -9,10 +9,11 @@ def read_words_from_file(file_path):
         words = [word.strip() for line in file for word in line.split()]
     return words
 
-def filter_csv_by_words(csv_file, words):
+def filter_csv_by_words_and_image(csv_file, words):
     df = pd.read_csv(csv_file)
     if words:
         df = df[~df['title'].str.lower().str.contains('|'.join(words), na=False)]
+    df = df.drop_duplicates(subset=['title', 'image_path'], keep='first')
     return df
 
 def save_filtered_csv(filtered_df, output_file):
@@ -33,7 +34,7 @@ for root, _, files in os.walk(sheets_folder):
             csv_file = os.path.join(root, file)
             print(f"Currently processing {file}")
 
-            filtered_df = filter_csv_by_words(csv_file, words_to_filter)
+            filtered_df = filter_csv_by_words_and_image(csv_file, words_to_filter)
 
             output_file = os.path.join(output_folder, file)
             save_filtered_csv(filtered_df, output_file)
